ffb34be7a3b08df1c6a66eb8d32ce494214b0560
filipe-vitorino <filipe0vit@gmail.com>
2025-06-02 20:15:52 -0300

    View model para o login

---
+++ lib/viewmodels/login_viewmodel.dart
@@ -0,0 +1,42 @@
+import 'package:academia/services/user_session.dart';
+import 'package:flutter/material.dart';
+import '../services/auth_service.dart';
+
+class LoginViewModel extends ChangeNotifier {
+  final TextEditingController usuarioController = TextEditingController();
+  final TextEditingController senhaController = TextEditingController();
+
+  final AuthService _authService = AuthService();
+
+  String? errorMessage;
+  bool isLoading = false;
+
+  Future<void> autenticar() async {
+    isLoading = true;
+    notifyListeners();
+
+    final sucesso = await _authService.login(
+      usuarioController.text.trim(),
+      senhaController.text,
+    );
+    isLoading = false;
+
+    if (sucesso != null) {
+      errorMessage = null;
+      sessao(sucesso);
+    } else {
+      errorMessage = 'Usuário ou senha inválidos';
+    }
+
+    notifyListeners();
+  }
+
+  bool sessao(int user_Id) {
+    if (user_Id != null) {
+      UserSession().login(user_Id);
+      return true;
+    }
+
+    return false;
+  }
+}
